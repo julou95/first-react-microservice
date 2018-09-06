8c3741bb079946924f8cd72b7aca3a0b480cf387
Louis Jungo <louis.jungo@gmail.com>
2018-09-06 16:18:04 +0200

    commit some giphys

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="https://media.giphy.com/media/C6PKnnzJ2Ywrm/giphy.gif" className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Jenkins X with React</h1>
         </header>
         <p className="App-intro">
-          <img src="https://media.giphy.com/media/xThtaevzbPFR2z0yEo/giphy.gif" alt="jej" />
           <img src="https://media.giphy.com/media/xThtaevzbPFR2z0yEo/giphy.gif" alt="jej" />
           <img src="https://media.giphy.com/media/xThtaevzbPFR2z0yEo/giphy.gif" alt="jej" />
           <img src="https://media.giphy.com/media/xThtaevzbPFR2z0yEo/giphy.gif" alt="jej" />
